49383a424ecc7af3342402d7c4413fbe451f1e5d
Kyaw Min Hein <kyawminhein101@gmail.com>
2025-06-29 22:11:13 +0700

    file_metadata

--- index.js
+++ index.js
@@ -11,8 +11,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  } else {
+    const fileInfo = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    };
+    return res.json(fileInfo);
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
